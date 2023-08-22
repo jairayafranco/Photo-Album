d7e37f9feb98b9a4fb6e391dd1d77f2649ee0882
Jair Aya <jair.aya@uniminuto.edu.co>
2023-08-22 12:25:36 -0500

    added select to add image to existing album

--- src/app/components/ui/add-album/add-to-album-dialog.tsx
+++ src/app/components/ui/add-album/add-to-album-dialog.tsx
@@ -1,17 +1,26 @@
 "use client"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useModalStore } from "@/app/store/useModalStore";
-import { addToAlbumAction } from "./actions";
+import { addToAlbumAction, getAlbums } from "./actions";
+import { Folder } from "../dashboard/Albums";
 
 export default function AddToAlbumDialog() {
     const [albumName, setAlbumName] = useState("")
+    const [folders, setFolders] = useState<Folder[]>([])
+    const [selectedFolder, setSelectedFolder] = useState("")
     const { image } = useModalStore()
+
+    useEffect(() => {
+        getAlbums().then((folders) => {
+            setFolders(folders)
+        })
+    }, [])
 
     return (
         <dialog id="dialog" className="modal">
             <form method="dialog" className="modal-box">
                 <section className="flex items-center justify-between">
-                    <h3 className="font-bold text-lg">Add to Album</h3>
+                    <h3 className="font-bold text-lg">Create New Album</h3>
                 </section>
                 <p className="py-4">
                     Type an album name to add this image to it.
@@ -37,9 +46,42 @@ export default function AddToAlbumDialog() {
                             setAlbumName("")
                         }}
                     >
-                        Add to Album
+                        Create Album
                     </button>
                 </div>
+
+                <section className="my-2">
+                    <h3 className="font-bold text-lg">Add To Existing Album</h3>
+
+                    <select
+                        className="select select-bordered w-full"
+                        onChange={(e) => setSelectedFolder(e.target.value)}
+                    >
+                        {folders.map((folder, index) => (
+                            <option
+                                key={index}
+                                value={folder.path}
+                                className="capitalize"
+                            >
+                                {folder.name}
+                            </option>
+                        ))}
+                    </select>
+
+                    <div className="modal-action">
+                        <button className="btn btn-primary" onClick={async () => {
+                            if (selectedFolder.trim() === "") {
+                                alert("Please select an album")
+                                return
+                            }
+                            await addToAlbumAction(selectedFolder, image)
+                            setSelectedFolder("")
+                        }}>
+                            Add to Album
+                        </button>
+                    </div>
+                </section>
+
             </form>
             <form method="dialog" className="modal-backdrop">
                 <button>close</button>
